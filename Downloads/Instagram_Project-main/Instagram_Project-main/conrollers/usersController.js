440199e3a2ec8c09f9513efdbdac5804b94a0008
RonLhayanie <ronlhayanie1@gmail.com>
2025-08-22 00:31:18 +0300

    Remove Downloads folder from repository

--- Downloads/Instagram_Project-main/Instagram_Project-main/conrollers/usersController.js
+++
@@ -1,9 +0,0 @@
-//create router
-const express = require('express')
-const router  = express.Router() 
-
-// add all needed http methods to the router
-// such as router.get('/create')
-// note: the final call will be /users/create so it doesnt need to be specific
-
-module.exports = router
--- Downloads/Instagram_Project-main/Instagram_Project-main/models/usersModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('./../db')
-
-// data base manipulation functions 
-// such as findUser() deleteUser()
-
-//export
-module.exports = {
-    // functions
-}
