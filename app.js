36eb6cc80f3c78cf110e8258e98b8461f75199c7
orbensh <orbensh09@gmail.com>
2025-09-06 22:39:15 +0300

    nothing changed

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-app.use(express.json({ limit: '200mb' }));
-app.use(express.urlencoded({ limit: '200mb', extended: true }));
 
 const usersRouter = require('./controllers/usersController')
 const chatsRouter = require('./controllers/chatsController')
